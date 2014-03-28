daaec88b0283993dc03b222f03a9adb29d98457f
super3 <me@super3.org>
2014-03-27 20:43:58 -0400

    Added Project Page

--- projects.php
+++ projects.php
@@ -11,18 +11,18 @@
 
       <div class="item">
         <img src="assets/img/projects/icon-translation.png" class="projects-icon" alt="Landing Page and Translations">
-        <h3><a href="">Landing Page and Translations</a></h3>
+        <h3><a href="http://www.peercointalk.org/index.php?topic=1665.0">Landing Page and Translations</a></h3>
         <p>Our website is only available on English language at the moment, which is an entry barrier for non-english speakers interested in Peercoin. A simple landing page will list the key features in as many lanugages as possible. </p>
-        <a href="" class="btn btn-primary btn-lg projects-button">Read More</a>
+        <a href="http://www.peercointalk.org/index.php?topic=1665.0" class="btn btn-primary btn-lg projects-button">Read More</a>
       </div>
     </div>
 
     <div class="col-md-5 col-md-offset-1">
       <div class="item">
         <img src="assets/img/projects/icon-merchant.png" class="projects-icon" alt="Merchant Page and Outreach Program">
-        <h3><a href="">Merchant Page and Outreach Program</a></h3>
+        <h3><a href="http://www.peercointalk.org/index.php?topic=2551">Merchant Page and Outreach Program</a></h3>
         <p>We need better integration documentation for merchants or our own solution if possible. We will also need to showcase successful merchants. Peercoin is not just for trading, people need to be able to spend it as well. This project merges with the Social Faucet project.
-        <a href="" class="btn btn-primary btn-lg projects-button">Read More</a>
+        <a href="http://www.peercointalk.org/index.php?topic=2551" class="btn btn-primary btn-lg projects-button">Read More</a>
       </div>
     </div>
     <hr>
@@ -33,18 +33,18 @@
 
       <div class="item">
         <img src="assets/img/projects/icon-wallet.png" class="projects-icon" alt="Online Web Wallet">
-        <h3><a href="">Online Web Wallet</a></h3>
+        <h3><a href="http://www.peercointalk.org/index.php?topic=2545">Online Web Wallet</a></h3>
         <p>Reduce the entry barrier by maintaining an online web wallet where only the user would have the private keys.</p>
-        <a href="" class="btn btn-primary btn-lg projects-button">Read More</a>
+        <a href="http://www.peercointalk.org/index.php?topic=2545" class="btn btn-primary btn-lg projects-button">Read More</a>
       </div>
     </div>
 
     <div class="col-md-5 col-md-offset-1">
       <div class="item">
         <img src="assets/img/projects/icon-android.png" class="projects-icon" alt="Android Moble App">
-        <h3><a href="">Android Moble App</a></h3>
+        <h3><a href="http://www.peercointalk.org/index.php?topic=2547">Android Moble App</a></h3>
         <p>To spread and simplify the usage of Peercoin in person, we would need mobile wallet on Android.</p>
-        <a href="" class="btn btn-primary btn-lg projects-button">Read More</a>
+        <a href="http://www.peercointalk.org/index.php?topic=2547" class="btn btn-primary btn-lg projects-button">Read More</a>
       </div>
     </div>
     <hr>
@@ -55,18 +55,18 @@
 
       <div class="item">
         <img src="assets/img/projects/icon-faucet.png" class="projects-icon" alt="Social Faucets">
-        <h3><a href="">Social Faucet</a></h3>
+        <h3><a href="http://www.peercointalk.org/index.php?topic=2548">Social Faucet</a></h3>
         <p>Lets automate the swarm process, and actually give users a reason to keep coming back to the Peercoin website. Users can earn crypto by rewarding swarm participants, participating in social media, referring friends to download the client, etc. This is not done anywhere else and it could grow our userbase exponentially.</p>
-        <a href="" class="btn btn-primary btn-lg projects-button">Read More</a>
+        <a href="http://www.peercointalk.org/index.php?topic=2548" class="btn btn-primary btn-lg projects-button">Read More</a>
       </div>
     </div>
 
     <div class="col-md-5 col-md-offset-1">
       <div class="item">
         <img src="assets/img/projects/icon-ads.png" class="projects-icon" alt="Direct Advertisement">
-        <h3><a href="">Direct Advertisement</a></h3>
+        <h3><a href="http://www.peercointalk.org/index.php?topic=2549">Direct Advertisement</a></h3>
         <p>We receive around 50,000 unique visitors per month. With a direct advertisement to the right place on our website, we can double or triple the amount of unique visitors.</p>
-        <a href="" class="btn btn-primary btn-lg projects-button">Read More</a>
+        <a href="http://www.peercointalk.org/index.php?topic=2549" class="btn btn-primary btn-lg projects-button">Read More</a>
       </div>
     </div>
     <hr>
@@ -77,18 +77,18 @@
 
       <div class="item">
         <img src="assets/img/projects/icon-website.png" class="projects-icon" alt="Website Fixes">
-        <h3><a href="">Website Fixes</a></h3>
+        <h3><a href="https://github.com/super3/peercoin.net/issues?milestone=1&state=open">Website Fixes</a></h3>
         <p>The current website has some obvious problems in terms of organization and flow.</p>
-        <a href="" class="btn btn-primary btn-lg projects-button">Read More</a>
+        <a href="https://github.com/super3/peercoin.net/issues?milestone=1&state=open" class="btn btn-primary btn-lg projects-button">Read More</a>
       </div>
     </div>
 
     <div class="col-md-5 col-md-offset-1">
       <div class="item">
         <img src="assets/img/projects/icon-ideas.png" class="projects-icon" alt="Website Fixes">
-        <h3><a href="">New Ideas</a></h3>
+        <h3><a href="http://www.peercointalk.org/index.php?topic=2550">New Ideas</a></h3>
         <p>Here you can add your ideas and suggestions.</p>
-        <a href="" class="btn btn-primary btn-lg projects-button">New Idea Topic</a>
+        <a href="http://www.peercointalk.org/index.php?topic=2550" class="btn btn-primary btn-lg projects-button">New Idea Topic</a>
       </div>
     </div>
     <hr>
