939bdeecb80a58cf8115e8f26d03c7e150f020d1
Alexander Harkness <bearbin@gmail.com>
2014-04-01 17:46:36 +0100

    Removed slideshow CSS and JS.
    It's not used and contributes to page weight, increasing load time.
    
    Part of the project to solve #71.

--- footer.php
+++ footer.php
@@ -92,64 +92,18 @@
         var secondsText = "<?php echo $Locale->getText("seconds_ago"); ?>";
     </script>
     <script src="assets/js/ticker.js"></script>
-	<script src="assets/js/jquery.themepunch.revolution.min.js"></script>
-	<script src="assets/js/jquery.themepunch.plugins.min.js"></script>
 	<script src="assets/js/peercoin.js"></script>
-	<script>
-	$(document).ready(function() {
-	//Slideshow
-	$('.banner').revolution({
-	delay:9000,
-	startwidth:1040,
-	startheight:463,
-	onHoverStop:"off",						// Stop Banner Timet at Hover on Slide on/off
 
-	thumbWidth:100,							// Thumb With and Height and Amount (only if navigation Tyope set to thumb !)
-	thumbHeight:50,
-	thumbAmount:3,
-
-	hideThumbs:0,
-	navigationType:"bullet",				// bullet, thumb, none
-	navigationArrows:"none",				// nexttobullets, solo (old name verticalcentered), none
-
-	navigationStyle:"round-old",			// round,square,navbar,round-old,square-old,navbar-old, or any from the list in the docu (choose between 50+ different item), custom
-
-
-	navigationHAlign:"center",				// Vertical Align top,center,bottom
-	navigationVAlign:"bottom",				// Horizontal Align left,center,right
-	navigationHOffset:-419,
-	navigationVOffset:72,
-
-	touchenabled:"on",						// Enable Swipe Function : on/off
-
-	stopAtSlide:-1,							// Stop Timer if Slide "x" has been Reached. If stopAfterLoops set to 0, then it stops already in the first Loop at slide X which defined. -1 means do not stop at any slide. stopAfterLoops has no sinn in this case.
-	stopAfterLoops:-1,						// Stop Timer if All slides has been played "x" times. IT will stop at THe slide which is defined via stopAtSlide:x, if set to -1 slide never stop automatic
-
-	hideCaptionAtLimit:0,					// It Defines if a caption should be shown under a Screen Resolution ( Basod on The Width of Browser)
-	hideAllCaptionAtLilmit:0,				// Hide all The Captions if Width of Browser is less then this value
-	hideSliderAtLimit:0,					// Hide the whole slider, and stop also functions if Width of Browser is less than this value
-
-
-	fullWidth:"on",
-
-	shadow:0								//0 = no Shadow, 1,2,3 = 3 Different Art of Shadows -  (No Shadow in Fullwidth Version !)
-	})
-
-	});
+	<!-- Drop down menu in minting -->
+	<script type='text/javascript'>
+		$(document).ready(function() {
+			$('.collapse').on('shown.bs.collapse', function() {
+				$(this).parent().find(".glyphicon-down").removeClass("glyphicon-down").addClass("glyphicon-up");
+			}).on('hidden.bs.collapse', function() {
+				$(this).parent().find(".glyphicon-up").removeClass("glyphicon-up").addClass("glyphicon-down");
+			});
+		});
 	</script>
-
-<!-- Drop down menu in minting -->
-<script type='text/javascript'>
-$(document).ready(function() {
-$('.collapse').on('shown.bs.collapse', function(){
-$(this).parent().find(".glyphicon-down").removeClass("glyphicon-down").addClass("glyphicon-up");
-}).on('hidden.bs.collapse', function(){
-$(this).parent().find(".glyphicon-up").removeClass("glyphicon-up").addClass("glyphicon-down");
-});
-});
-</script>
-
-
 
   </body>
 </html>
