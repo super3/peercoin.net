448c047f0932889e73c50ec0db97b8845a29e214
Igor Rinkovec <shiqqqi@gmail.com>
2014-06-15 10:00:44 +0200

    Revert "Simplified homepage header."
    This reverts commit a4535733f971d11be4f4e7bd4112daba6faea6a1.

--- index.php
+++ index.php
@@ -5,9 +5,32 @@
 
   <!-- Headerwrap -->
 		<div class="row" style="max-width:100%; margin:0px;">
-			<h1>
+			<h1 style="margin-top:90px;">
 				<?php echo $Locale->getText("big_welcome_header"); ?>
 	    	</h1>
+			<a href="#whypeercoin" class="btn btn-embossed btn-primary smoothScroll"><?php echo $Locale->getText("why_peercoin_button"); ?></a>
+	        <a href="downloads" target="_blank" class="btn btn-embossed btn-primary"><?php echo $Locale->getText("download_wallet"); ?>!</a>
+
+	        <div class="marketcap">
+		        <ul>
+			        <li>
+				        <span id="current-price"></span>
+			        	<span><?php echo $Locale->getText("price"); ?></span>
+			        </li>
+			        <li>
+				        <span id="market-cap"></span>
+				        <span><?php echo $Locale->getText("market_cap"); ?></span>
+			        </li>
+			        <li>
+				        <span id="total-supply"></span>
+				        <span><?php echo $Locale->getText("total_supply"); ?></span>
+			        </li>
+		        </ul>
+		        <div class="updated">
+			        <span><?php echo $Locale->getText("ticker_last_updated"); ?></span>
+	    	        <span id="seconds-since-update"><?php echo $Locale->getText("ticker_last_updated_never"); ?></span>
+		        </div>
+	        </div>
 		</div>
 	</header>
 
