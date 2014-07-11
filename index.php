3b76bc56edf15f8f13c1651364cd639c3ad97e92
Igor Rinkovec <shiqqqi@gmail.com>
2014-07-11 17:27:49 +0200

    Added wallet to the header menu, and fixed the wallet index link.

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 					<img src="assets/img/logos/peercoin_ltext.png" alt="peercoin" />
 					<br><br>
 					<a href="" class="btn btn-primary"><?php echo $Locale->getText("why_peercoin_title"); ?></a>
-					<a href="" class="btn btn-primary"><?php echo $Locale->getText("download_wallet"); ?></a>
+					<a href="wallet" class="btn btn-primary"><?php echo $Locale->getText("download_wallet"); ?></a>
 				</div>
 	    	</div>
 		</div>
