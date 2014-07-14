2241b758835b860f5ffa67de937f3b04e2590c8c
Igor Rinkovec <shiqqqi@gmail.com>
2014-07-14 20:14:26 +0200

    Exchange tabs redesigned and added Cryptsy.

--- exchanges.php
+++ exchanges.php
@@ -5,8 +5,8 @@
 	</div><!--
 	--><div class="col-md-5 col-md-offset-1 vcenter">
 		<h2>What are exchanges?</h2>
-		<p><b>Exchanges</b> allow its users to exchange (hence the name) one currency for another. 
-		Using them you can use your govenment backed currency (such as USD or EUR) on your bank 
+		<p><b>Exchanges</b> allow their users to exchange (hence the name) one currency for another. 
+		Using them you can transfer your government-backed currency (such as USD or EUR) from your bank 
 		account to buy Peercoin or sell Peercoin back to that currency.</p>
 	</div>
 </div>
@@ -16,9 +16,9 @@
 		<h2>How do they work?</h2>
 		<p>
 			To exchange one currency for another, first you are required to <b>deposit</b> the currency you currently own to an exchange. 
-			As soon as the exchange recieve your money (or it passes a set number of confirmations in the case of cryptocurrency), 
+			As soon as the exchange recieves your money (or it passes a set number of confirmations in the case of cryptocurrency), 
 			the exchange should register the amount in their system and you should be able to start trading between currencies. 
-			At this state your money is just a number in the exchanges system. When you are done trading you can recieve real 
+			At this state your money is just a number in the exchange's system. When you are done trading you can receive real 
 			currency by <b>withdraw</b>ing it from the exchange.
 		</p>
 	</div><!--
@@ -37,7 +37,7 @@
 			The actual exchange process is done by creating <b>Buy</b> and <b>Sell</b> orders. In a Peercoin exchange, by creating a <b>Buy</b> order 
 			you are actually requesting to buy a certain amount of Peercoins for a lower price. And by creating a <b>Sell</b> order you are requesting 
 			to sell Peercoins for a higher price. When different orders overlap in prices an agreement has been made and the trade happens automatically. 
-			Accounts of both parties get automatically get credited with the coins they traded.
+			Accounts of both parties automatically get credited with the coins they traded.
 		</p>
 		<p>
 			You can also choose to Buy/Sell at a <b>Market Price</b> which will do the trade immediately at the best price currently available on the exchange.
@@ -49,7 +49,7 @@
 	<div class="col-md-5 col-md-offset-1 vcenter">
 		<h2>Fees</h2>
 		<p>
-			When a trade happens a small percentage by the exchange for facilitating the transaction. This is known as a <b>Trading Fee</b>.
+			When a trade happens a small percentage is taken by the exchange for facilitating the transaction. This is known as a <b>Trading Fee</b>.
 		</p>
 		<p>
 			Some exchanges charge so called <b>Withdrawal Fees</b> each time you withdraw currency. Sometimes these are caused by the actual 
@@ -77,6 +77,7 @@
 			<a href="#" class="exchange-button active" data-toggle="tooltip" data-placement="top" title="Coinomat" data-exchange="coinomat"><img src="assets/img/exchanges/logos/coinomat.png"></a>
 			<a href="#" class="exchange-button" data-toggle="tooltip" data-placement="top" title="Vault of Satoshi" data-exchange="vault-of-satoshi"><img src="assets/img/exchanges/logos/vault-of-satoshi.png"></a>
 			<a href="#" class="exchange-button" data-toggle="tooltip" data-placement="top" title="BTCe" data-exchange="btce"><img src="assets/img/exchanges/logos/btce.png"></a>
+			<a href="#" class="exchange-button" data-toggle="tooltip" data-placement="top" title="Cryptsy" data-exchange="cryptsy"><img src="assets/img/exchanges/logos/cryptsy.png"></a>
 			<a href="#" class="exchange-button" data-toggle="tooltip" data-placement="top" title="BitTrex" data-exchange="bittrex"><img src="assets/img/exchanges/logos/bittrex.png"></a>
 			<a href="#" class="exchange-button" data-toggle="tooltip" data-placement="top" title="The Rock" data-exchange="therock"><img src="assets/img/exchanges/logos/therock.png"></a>
 			<a href="#" class="exchange-button" data-toggle="tooltip" data-placement="top" title="Bittylicious" data-exchange="bittylicious"><img src="assets/img/exchanges/logos/bittylicious.png"></a>
@@ -84,39 +85,136 @@
 		</div>
 		<div class="col-md-12">
 			<div id="exchange-coinomat" class="exchange-info active">
-				<h3>Coinomat</h3>
-				<p>Quick & Easy. No registration required.</p>
-				<a class="btn btn-primary btn-large btn-block" target="_blank" href="https://coinomat.com/">Visit</a>
+				<div class="col-lg-6 col-lg-offset-3">
+					<h3><b><img src="assets/img/exchanges/logos/coinomat.png"> Coinomat</b></h3>
+					<table>
+						<tr>
+							<td style="padding: 0px 15px 0px 15px"><b>Location:</b></td>
+							<td>British Virgin Islands</td>
+						</tr>
+						<tr>
+							<td style="padding: 0px 15px 0px 15px"><b>Notes:</b></td>
+							<td>The exchange allows you to trade instantly in market price. No registration is required, making it simple to use.</td>
+						</tr>
+					</table>
+					<a class="btn btn-primary btn-large btn-block" target="_blank" href="https://coinomat.com/">Visit</a>
+				</div>
 			</div>
 			<div id="exchange-vault-of-satoshi" class="exchange-info">
-				<h3>Vault of Satoshi</h3>
-				<p>Well designed, large exchange.</p>
-				<a class="btn btn-primary btn-large btn-block" target="_blank" href="https://www.vaultofsatoshi.com/">Visit</a>
+				<div class="col-lg-6 col-lg-offset-3">
+					<h3><b><img src="assets/img/exchanges/logos/vault-of-satoshi.png" height="50" width="50"> Vault of Satoshi</b></h3>
+					<table>
+						<tr>
+							<td style="padding: 0px 15px 0px 15px"><b>Location:</b></td>
+							<td>Canada</td>
+						</tr>
+						<tr>
+							<td style="padding: 0px 15px 0px 15px"><b>Notes:</b></td>
+							<td>Well designed and quality exchange.</td>
+						</tr>
+					</table>
+					<a class="btn btn-primary btn-large btn-block" target="_blank" href="https://www.vaultofsatoshi.com/">Visit</a>
+				</div>
 			</div>
 			<div id="exchange-btce" class="exchange-info">
-				<h3>BTCe</h3>
-				<p>WARNING: 3-day withdraw delay for new users</p>
-				<a class="btn btn-primary btn-large btn-block" target="_blank" href="https://btc-e.com/">Visit</a>
+				<div class="col-lg-6 col-lg-offset-3">
+					<h3><b><img src="assets/img/exchanges/logos/btce.png"> BTCe</b></h3>
+					<table>
+						<tr>
+							<td style="padding: 0px 15px 0px 15px"><b>Location:</b></td>
+							<td>Bulgaria</td>
+						</tr>
+						<tr>
+							<td style="padding: 0px 15px 0px 15px"><b>Notes:</b></td>
+							<td>
+								Highest volume PPC exchange.
+								<br>
+								Warning: Withdrawal for new users on this exchange is available only after 3 days.
+							</td>
+						</tr>
+					</table>
+					<a class="btn btn-primary btn-large btn-block" target="_blank" href="https://btc-e.com/">Visit</a>
+				</div>
+			</div>
+			<div id="exchange-cryptsy" class="exchange-info">
+				<div class="col-lg-6 col-lg-offset-3">
+					<h3><b><img src="assets/img/exchanges/logos/cryptsy.png"> Cryptsy</b></h3>
+					<table>
+						<tr>
+							<td style="padding: 0px 15px 0px 15px"><b>Location:</b></td>
+							<td>United States</td>
+						</tr>
+						<tr>
+							<td style="padding: 0px 15px 0px 15px"><b>Notes:</b></td>
+							<td>-</td>
+						</tr>
+					</table>
+					<a class="btn btn-primary btn-large btn-block" target="_blank" href="https://www.cryptsy.com/">Visit</a>
+				</div>
 			</div>
 			<div id="exchange-bittrex" class="exchange-info">
-				<h3>BitTrex</h3>
-				<p>US based exchange.</p>
-				<a class="btn btn-primary btn-large btn-block" target="_blank" href="https://bittrex.com/">Visit</a>
+				<div class="col-lg-6 col-lg-offset-3">
+					<h3><b><img src="assets/img/exchanges/logos/bittrex.png"> BitTrex</b></h3>
+					<table>
+						<tr>
+							<td style="padding: 0px 15px 0px 15px"><b>Location:</b></td>
+							<td>United States</td>
+						</tr>
+						<tr>
+							<td style="padding: 0px 15px 0px 15px"><b>Notes:</b></td>
+							<td>-</td>
+						</tr>
+					</table>
+					<a class="btn btn-primary btn-large btn-block" target="_blank" href="https://bittrex.com/">Visit</a>
+				</div>
 			</div>
 			<div id="exchange-therock" class="exchange-info">
-				<h3>The Rock</h3>
-				<p>On the scene for more then 7 years.</p>
-				<a class="btn btn-primary btn-large btn-block" target="_blank" href="http://www.therocktrading.com/">Visit</a>
+				<div class="col-lg-6 col-lg-offset-3">
+					<h3><b><img src="assets/img/exchanges/logos/therock.png"> The Rock</b></h3>
+					<table>
+						<tr>
+							<td style="padding: 0px 15px 0px 15px"><b>Location:</b></td>
+							<td>Malta</td>
+						</tr>
+						<tr>
+							<td style="padding: 0px 15px 0px 15px"><b>Notes:</b></td>
+							<td>On the scene for more than 7 years.</td>
+						</tr>
+					</table>
+					<a class="btn btn-primary btn-large btn-block" target="_blank" href="http://www.therocktrading.com/">Visit</a>
+				</div>
 			</div>
 			<div id="exchange-bittylicious" class="exchange-info">
-				<h3>Bittylicious</h3>
-				<p>Simple to use, no registration.</p>
-				<a class="btn btn-primary btn-large btn-block" target="_blank" href="https://bittylicious.com/">Visit</a>
+				<div class="col-lg-6 col-lg-offset-3">
+					<h3><b><img src="assets/img/exchanges/logos/bittylicious.png"> Bittylicious</b></h3>
+					<table>
+						<tr>
+							<td style="padding: 0px 15px 0px 15px"><b>Location:</b></td>
+							<td>England</td>
+						</tr>
+						<tr>
+							<td style="padding: 0px 15px 0px 15px"><b>Notes:</b></td>
+							<td>-</td>
+						</tr>
+					</table>
+					<a class="btn btn-primary btn-large btn-block" target="_blank" href="https://bittylicious.com/">Visit</a>
+				</div>
 			</div>
 			<div id="exchange-btc38" class="exchange-info">
-				<h3>BTC38</h3>
-				<p>Chineese exchange</p>
-				<a class="btn btn-primary btn-large btn-block" target="_blank" href="http://www.btc38.com/trade.html?btc38_trade_coin_name=ppc">Visit</a>
+				<div class="col-lg-6 col-lg-offset-3">
+					<h3><b><img src="assets/img/exchanges/logos/btc38.png"> BTC38</b></h3>
+					<table>
+						<tr>
+							<td style="padding: 0px 15px 0px 15px"><b>Location:</b></td>
+							<td>China</td>
+						</tr>
+						<tr>
+							<td style="padding: 0px 15px 0px 15px"><b>Notes:</b></td>
+							<td>-</td>
+						</tr>
+					</table>
+					<a class="btn btn-primary btn-large btn-block" target="_blank" href="http://www.btc38.com/trade.html?btc38_trade_coin_name=ppc">Visit</a>
+				</div>
 			</div>
 		</div>
 	</div>
