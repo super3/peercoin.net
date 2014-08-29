2e04731b62dc6dff8eeceda118f5a0fa925a60a5
Igor Rinkovec <shiqqqi@gmail.com>
2014-08-29 12:13:15 +0200

    Implemented mining guide.

---
+++ mining-guide.php
@@ -0,0 +1,58 @@
+<?php  $page_title = "Mining Guide"; include ('header.php'); ?>
+
+	<div class="row text-center">
+		<img class="svg-quickstart" src="assets/img/misc/quick-start.svg" />
+	</div>
+
+	<div class="row row-nomargin">
+		<div class="col-md-12 text-center">
+			<h2>Follow these steps to start mining Peercoin!</h2>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-lg-8 col-lg-offset-2">
+			<ol>
+				<li>
+					<b>Set up the wallet software</b>
+					<br>
+					If you haven't yet, download the <a href="wallet">Peercoin wallet</a>.
+				</li>
+				<li>
+					<b>Select a mining pool</b>
+					<div class="row">
+						<div class="col-md-6">
+							<h4 class="nomargin">Peercoin-dedicated pools:</h4>
+							<ul>
+							    <li>
+							    	<a href="https://peercoin.ecoining.com/">Ecoining</a> -  1% fee, Dynamic PPLNS Payment System
+							    </li>
+							    <li>
+							    	<a href="https://ppcoin.d7.lt/">D7</a> -  1.5% fee, PPLNS: payout per share of the last N rounds
+							    </li>
+							    <li>
+							    	<a href="http://ppc.fixx.ru/">Fixx</a> -  Donation only, PPLNS: payout per share of the last N rounds
+							    </li>
+						    </ul>
+						</div>
+						<div class="col-md-6">
+							<h4 class="nomargin">Multicoin pools:</h4>
+							<ul>
+      							<li>
+        							<a href="https://www.coinotron.com/coinotron/">Coinotron</a> - Pay Per Share: 12% fee, Round-Based Pay Per Share: 2%
+      							</li>
+      							<li>
+        							<a href="http://www.multipool.us/">Multipool.us</a> - 1.5% fee, Pay Per Share
+      							</li>
+    						</ul>
+						</div>
+					</div>
+				</li>
+				<li>
+					<b>Sign up with the pool</b><br>
+					Follow the instructions on the pool website. Below are tutorials for a few specific pools.
+				</li>
+			</ol>
+		</div>
+	</div>
+</div>
+<?php include ('footer.php'); ?>
