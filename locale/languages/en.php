ede184b3cf45cf2d1a1f41d991a3324693ba0f10
MAL315 <MAL315@users.noreply.github.com>
2016-10-04 22:19:08 -0400

    Update en.php

--- locale/languages/en.php
+++ locale/languages/en.php
@@ -153,7 +153,7 @@ $lang['en'] += array(
 	'investors.efficient_security' => 'Efficient Security',
 	'investors.efficient_security_desc' => 'Peercoin is the first proof-of-stake coin, which means it doesn’t require massive computing power to secure the network. Get into the nitty-gritty with the <a href="whitepaper">Peercoin whitepaper</a>.',
 	'investors.guaranteed_returns' => 'Guaranteed Returns',
-	'investors.guaranteed_returns_desc' => 'After Peercoins are held for 30 days, they are eligible to earn a 1&#37; annualized reward. <a href="minting">Learn more about minting</a> or you can also buy coins from one of the many <a href="exchanges">Peercoin exchange</a>.',
+	'investors.guaranteed_returns_desc' => 'After Peercoins are held for 30 days, they are eligible to earn a 1&#37; annualized reward. <a href="minting">Learn more about minting</a> or you can also get started by buying coins from one of the many <a href="exchanges">Peercoin exchange</a>.',
 
 );
 
