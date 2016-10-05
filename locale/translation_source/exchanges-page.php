3581c52ce7325ab82f1d707474e28016383187d2
MAL315 <MAL315@users.noreply.github.com>
2016-10-04 21:48:58 -0400

    Update exchanges-page.php

--- locale/translation_source/exchanges-page.php
+++ locale/translation_source/exchanges-page.php
@@ -6,7 +6,7 @@ $lang['en'] += array(
 	'exchanges.buy_sell_desc' => 'Trades begin by the placing of buy and sell orders. Create a <b>Buy order</b> to request a certain amount of Peercoins for a specific price. Create a <b>Sell order</b> to offer to sell Peercoins. When different orders overlap in price, the trade is automatically completed, and the accounts of both parties are credited with the currencies they traded. A <b>Market Order</b> will trade immediately at the price currently available.',
 	'exchanges.fees' => 'Fees',
 	'exchanges.fees_desc' => 'Each time a trade occurs, a small percentage is taken by the exchange for facilitating the transaction. This is known as a <b>Trading Fee</b>. Some exchanges also charge a <b>Withdrawal Fee</b> when you withdraw your new currency from the exchange.',
-	'exchanges.choosing' => 'Find a Peercoin exchange at <a href="http://coinmarketcap.com/currencies/peercoin/#markets">coinmarketcap.com</a>',
+	'exchanges.choosing' => '<b>Find a Peercoin exchange using <a href="http://coinmarketcap.com/currencies/peercoin/#markets">CoinMarketCap</a></b>',
 	'exchanges.disclaimer' => 'Reminder: Be sure to withdraw your Peercoins from the exchange after you purchase them. Storing coins in your own wallet is more secure, and makes you eligible for Peercoin’s 1&#37; minting reward.',
 	'exchanges.location' => 'Location',
 	'exchanges.notes' => 'Notes',
