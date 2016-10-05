f9b74bd9a8ad49621a6bba65c5c92cdfbbb57d5d
MAL315 <MAL315@users.noreply.github.com>
2016-10-04 22:46:36 -0400

    Update en.php

--- locale/languages/en.php
+++ locale/languages/en.php
@@ -146,7 +146,7 @@ $lang['en'] += array(
 	'investors.established_network' => 'Established Network',
 	'investors.established_network_desc' => 'Peercoin was launched in 2012, making it one of the oldest and most reliable cryptocoin networks in existence.',
 	'investors.limited_supply' => 'Limited Supply',
-	'investors.limited_supply_desc' => 'Peercoin’s annual inflation rate has averaged below 5&#37; during the year 2014, and continues to fall. See <a href="http://peerchain.net/charts.html">peerchain.net</a> for detailed historic data.',
+	'investors.limited_supply_desc' => 'Peercoin’s annual inflation rate has averaged below 5&#37; during the year 2014, and continues to fall.',
 	'investors.efficient_security' => 'Efficient Security',
 	'investors.efficient_security_desc' => 'Peercoin is the first proof-of-stake coin, which means it doesn’t require massive computing power to secure the network. Get into the nitty-gritty with the <a href="whitepaper">Peercoin whitepaper</a>.',
 	'investors.guaranteed_returns' => 'Guaranteed Returns',
