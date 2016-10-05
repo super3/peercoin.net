d9642efd32043985f14f9a1eb835b7cd07a3e165
MAL315 <MAL315@users.noreply.github.com>
2016-10-04 22:49:56 -0400

    Update minting.php

--- minting.php
+++ minting.php
@@ -39,15 +39,6 @@
 		</div>
 	</div>
 	<br><br>
-	<div class="row">
-		<div class="col-md-6 col-md-offset-3">
-			<div class="videoWrapper">
-			    <!-- Copy & Pasted from YouTube -->
-			    <iframe width="560" height="315" src="//www.youtube.com/embed/28294f8GTM0" frameborder="0" allowfullscreen></iframe>
-			</div>
-		</div>
-	</div>
-	<br>
 	<div class="row text-center">
 		<a href="minting-guide"><img class="svg-quickstart" src="assets/img/misc/minting-guide.png"></a>
 	</div>
